a54a276a2c114b1ccb5114a3257623356c1caf45
Stu67nt <tanzilchowdhury2014@gmail.com>
2026-07-14 19:15:51 +0100

    fixed broken hash storing and added support for multiple pfp updates at once.

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ import urllib
 from PIL import Image, ImageSequence
 import requests
 import time
+import random
 
 load_dotenv(override=True)
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-def upload_emoji(name):
+def upload_emoji(name, filename):
 	token = os.environ.get("UPLOAD_TOKEN")
-	headers = {
-		"Authorization": f"Bearer {token}"}
-	args = {
-		"name": name
-			}
-	with open("output.gif", "rb") as f:
+	headers = {"Authorization": f"Bearer {token}"}
+	args = {"name": name}
+	with open(filename, "rb") as gif:
 		x=requests.post("https://hackclub-slack-emoji-proxy.vercel.app/api/emoji/upload",
-						data = args, headers=headers, files={"file":f})
+						data = args, headers=headers, files={"file":gif})
+		gif.close()
 	print(x.text)
 
 
 def find_valid():
 	client = app.client
+	# Known bug where technicaly if more than 1000 users & bots join the channel it could cause issues but idc tbh.
 	lst = client.conversations_members(
 		channel="C0BH4B8HZ3K",
 		limit=1000
@@ -46,33 +46,42 @@ def handle_user_change(event, client, say):
 	valid_ids = find_valid()
 	user_id = event["user"]["id"]
 	display_name = event["user"]["profile"]["display_name"].translate(str.maketrans('', '', string.punctuation)).lower()
+
 	if user_id in valid_ids:
 		print(event)
 		url = event["user"]["profile"]["image_original"]
 		pfp_hash = event["user"]["profile"]["avatar_hash"]
 
-		with open("cache.txt", "r+") as f:
-			if f"{hash}\n" not in f.readlines():
+		with open("cache.txt", "r+") as cache:
+			if f"{pfp_hash}\n" not in cache.readlines():
 				print("new hash detected")
+				file_num = random.randint(100000, 999999)
 				image = urllib.request.urlopen(url).read()
-				with open("temp.png", 'wb') as file:
-					file.write(image)
-				pfp = Image.open("temp.png").resize((150, 150)).convert("RGBA")
+				with open(f"{file_num}.png", 'wb') as f:
+					f.write(image)
+
+				pfp = Image.open(f"{file_num}.png").resize((150, 150)).convert("RGBA")
 				pet = Image.open("pet.gif")
+
 				final_frames = []
 				for frame in ImageSequence.Iterator(pet):
 					frame = frame.resize((100, 100)).convert("RGBA")
 					pfp_copy = pfp.copy()
 					pfp_copy.paste(frame, mask=frame)
 					final_frames.append(pfp_copy)
-				final_frames[0].save("output.gif", save_all=True, append_images=final_frames[1:], loop=0)
-				upload_emoji(f"pet-{display_name}{int(time.time())}")
-				f.write(f"{hash}\n")
-				f.close()
-				os.remove("temp.png")
-				os.remove("output.gif")
+				final_frames[0].save(f"{file_num}.gif", save_all=True, append_images=final_frames[1:], loop=0)
+				emoji_name = f"pet-{display_name}{int(time.time())}"
+				upload_emoji(emoji_name, f"{file_num}.gif")
+
+				cache.write(f"{pfp_hash}\n")
+
+				os.remove(f"{file_num}.png")
+				os.remove(f"{file_num}.gif")
+				print("uplaoded")
+				app.client.chat_postMessage(channel="C0BH4B8HZ3K", text=f"<@{user_id}> :{emoji_name}:")
 			else:
 				print("found in cache")
+			f.close()
 
 if __name__ == "__main__":
 	print("starting")
